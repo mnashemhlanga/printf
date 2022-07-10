d397456e0a5db0e1039939865531de8026792335
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-07-10 03:11:53 -0700

    Add base implementation supporting strings and chars

---
+++ printf.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdarg.h>
+#include<string.h>
+#include<unistd.h>
+#include "main.h"
+
+/**
+ * _printf - custom implementation of the printf function
+ * @format: string containing the format specifiers
+ * Return: length of printed string
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i = 0, j = 0;
+	char *buff, *temp;
+
+	buff = malloc(sizeof(char *) * strlen(format)), temp = malloc(20);
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					{
+						buff[j] = (char)va_arg(ap, int);
+						j++;
+						break;
+					}
+				case 's':
+					{
+						temp = va_arg(ap, char *);
+						strcpy(&buff[j], temp);
+						j += strlen(temp);
+						break;
+					}
+			}
+		}
+		else
+		{
+			buff[j] = format[i];
+			j++;
+		}
+		i++;
+	}
+	write(1, buff, j);
+	va_end(ap);
+	return (j);
+}
