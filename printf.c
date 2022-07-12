b6eb8f810242071e8fd3693e56462372f9bc23ee
mnashemhlanga <mnashemhlanga8@gmail.com>
2022-07-11 20:16:42 -0700

    Update on printf

--- printf.c
+++ printf.c
@@ -6,52 +6,84 @@
 #include "main.h"
 
 /**
- * _printf - custom implementation of the printf function
- * @format: string containing the format specifiers
- * Return: length of printed string
+ * _printf - print any formated string
+ * @format: string format
+ * Return: the number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int i = 0, j = 0;
-	char *buff, *temp;
+	va_list list;
+	int count = 0, printed = 0;
 
-	buff = malloc(sizeof(char *) * strlen(format)), temp = malloc(20);
-	va_start(ap, format);
-
-	while (format && format[i])
+	if (!format)
+		return (-1);
+	va_start(list, format);
+	while (format && format[count])
 	{
-		if (format[i] == '%')
+		if (format[count] == '%')
 		{
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-					{
-						buff[j] = (char)va_arg(ap, int);
-						j++;
-						break;
-					}
-				case 's':
-					{
-						temp = va_arg(ap, char *);
-						strcpy(&buff[j], temp);
-						j += strlen(temp);
-						break;
-					}
-			}
+			if (format[count + 1] == '\0')
+				return (-1);
+			format_values(list, format, &printed, &count);
 		}
 		else
 		{
-			buff[j] = format[i];
-			j++;
+			_putchar(format[count]);
+			printed += 1;
+			count += 1;
 		}
-		i++;
 	}
-	fwrite(buff, j, 1, stdout);
-	va_end(ap);
-	free(temp);
-	free(buff);
-	return (j);
+	va_end(list);
+	return (printed);
 }
+/**
+ * format_values - format string
+ * @list: list of args
+ * @format: format string
+ * @printed: number of chars printed
+ * @count: count iterator
+ * Return: pointer to func that correspond to operator
+ */
+void format_values(va_list list, const char *format, int *printed, int *count)
+{
+	int f = 0, tobi = 0, tooc = 0;
+	unsigned int num = 0;
+
+	switch (format[*count + 1])
+	{
+		case '%':
+			_putchar(format[*count + 1]);
+			*printed += 1;
+			break;
+		case 'c':
+			_putchar(va_arg(list, int));
+			*printed += 1;
+			break;
+		case 's':
+			format_string(list, printed, 's');
+			break;
+		case 'd': case 'i':
+			format_int(list, printed);
+			break;
+		
+		case 'b':
+			num = va_arg(list, unsigned int);
+			tobi = _tobinoct(num, 0, 2);
+			*printed  += tobi;
+			break;
+		default:
+			*count += 1;
+			*printed += 1;
+			_putchar('%');
+			f = 1;
+	}
+	if (!f)
+		*count += 2;
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always -0
+ */
+
+		
