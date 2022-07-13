f334deff267a728ca4403e909e5cacbf8260ff22
mnashemhlanga <mnashemhlanga8@gmail.com>
2022-07-12 20:04:17 -0700

    additions and updates

---
+++ numlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _numlen - Calculates number of digits of a number
+ * @n: Passed number
+ * Return: Returns teh number of digits of a number.
+ */
+int _numlen(int n)
+{
+	int cnt = 0;
+	{
+		while (n != 0)
+			cnt++;
+	}
+
+	return (cnt);
+}
+
